d4324410dfff820095a02edc4a47076c4b569ad4
Noiddd <dionang11@gmail.com>
2022-10-19 17:52:07 +0800

    commit

--- Angular/demo/src/app/app.module.ts
+++ Angular/demo/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TestComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
