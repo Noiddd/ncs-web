75f3fa0a91883531cab6bc121ce00e5c1dfe76a7
Noiddd <dionang11@gmail.com>
2022-10-31 17:12:42 +0800

    forms

--- Angular/routing-project/src/app/app-routing.module.ts
+++ Angular/routing-project/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: 'allEmpData', component: AllEmpDataComponent },
+  { path: 'empDeptList', component: EmpDeptListComponent },
+  { path: 'empList', component: EmpListComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
